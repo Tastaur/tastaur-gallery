c45ab2ffb336d0786e9bfe6848ba3cb293a5cb6b
Mark <Trrrrex93@gmail.com>
2020-07-17 01:50:17 +0400

    Updates

---
+++ precache-manifest.40574ed2175982d60fc1afd263f1e0a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a333d55ecb238b3f398d670d58a721",
+    "url": "/tastaur-gallery/index.html"
+  },
+  {
+    "revision": "da741c1e3c7e726ed370",
+    "url": "/tastaur-gallery/static/css/main.faec43a2.chunk.css"
+  },
+  {
+    "revision": "7d9e56a2bcdbcf60c3fe",
+    "url": "/tastaur-gallery/static/js/2.9e87992c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tastaur-gallery/static/js/2.9e87992c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da741c1e3c7e726ed370",
+    "url": "/tastaur-gallery/static/js/main.7d01886e.chunk.js"
+  },
+  {
+    "revision": "46d562a70b697f8b3f73",
+    "url": "/tastaur-gallery/static/js/runtime-main.89760f57.js"
+  },
+  {
+    "revision": "7f2b49694ac76e5f058ebac4005f5571",
+    "url": "/tastaur-gallery/static/media/YanoneKaffeesatz-Regular.7f2b4969.ttf"
+  },
+  {
+    "revision": "e5a81464ea808b2f85188b8210042a79",
+    "url": "/tastaur-gallery/static/media/garbage.e5a81464.png"
+  }
+]);
