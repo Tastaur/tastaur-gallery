e481d14c085fc725d9b394fdc86e399a6db435a0
Mark <Trrrrex93@gmail.com>
2020-07-17 20:55:27 +0400

    clear code

--- src/component/Item/Item.jsx
+++ src/component/Item/Item.jsx
@@ -5,11 +5,9 @@ import s from './item.module.css'
 const Item = (props) => {
   let closeImg = (e) => {
     if (e.target.tagName !== 'IMG') {
-
       props.history.goBack()
     }
   }
-
   return <div onClick={closeImg} className={s.container}>
     {props.gallery.showImage ? <img src={props.gallery.showImage[0].src} className={s.img} alt=''/> : <Redirect to={`/`}/>}
   </div>
